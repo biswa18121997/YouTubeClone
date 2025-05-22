1c07236403ab973f1522a5f3092aa031c9da9bc1
biswa18121997 <biswajitshrm6@gmail.com>
2025-05-22 07:24:02 +0530

    add few componenets and integrated YT API

--- YT_Clone_Frontend/src/components/Register.jsx
+++ YT_Clone_Frontend/src/components/Register.jsx
@@ -32,12 +32,12 @@ export default function Register(){
             <form action="/login" className="flex flex-col justify-center items-center border p-4 rounded-2xl w-2/3">
                 <i className="fa-solid fa-user  p-2 m-2 rounded-2xl text-8xl border-2 text-center"></i>
                 <label htmlFor="registername" onChange={(e)=>setName(e.target.value)} className="text-start">Enter Full Name :</label>
-                <input type='text'  name="name" id="registername" placeholder="Enter Your Full Name." className="p-2 bg-neutral-400 w-full rounded-3xl m-2" />
-                <label htmlFor="loginmail" onChange={(e)=>setMail(e.target.value)} className="">Registered  Email :</label>
-                <input type='email'  name="email" id="loginmail" placeholder="Enter Your Registered E-mail.." className="p-2 bg-neutral-400 w-full rounded-3xl m-2" />
+                <input type='text'  name="name" id="registername" onChange={(e)=>setName(e.target.value)} placeholder="Enter Your Full Name." className="p-2 bg-neutral-400 w-full rounded-3xl m-2" />
+                <label htmlFor="loginmail" className="">Registered  Email :</label>
+                <input type='email'  name="email" id="loginmail" onChange={(e)=>setMail(e.target.value)} placeholder="Enter Your Registered E-mail.." className="p-2 bg-neutral-400 w-full rounded-3xl m-2" />
                 <label htmlFor="loginpassword">Password :</label>
                 <section className="w-full flex justify-center items-center">
-                    <input type={show?'text':'password'} name="password" id="loginpassword" placeholder="Enter your password " className="p-2 bg-neutral-400 w-full rounded-3xl m-2"/>
+                    <input type={show?'text':'password'} name="password" id="loginpassword" onChange={(e)=>setPassword(e.target.value)} placeholder="Enter your password " className="p-2 bg-neutral-400 w-full rounded-3xl m-2"/>
                     <i onClick={()=>setShow(!show)} class={show?'fa-solid fa-eye':'fa-solid fa-eye-low-vision' }></i>
                 </section>
                 <p className="text-blue-700 underline underline-offset-8 m-1">Forgot your Password .? </p>         
